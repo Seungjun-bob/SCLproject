e8e1e05ff0b8e7d3f0d1c5d8718ac552d6b23f69
Seungjub-bob <rkdtmdwns0105@gmail.com>
2022-02-26 02:26:05 +0900

    test bootstrap on index.html

--- SCLproject/settings.py
+++ SCLproject/settings.py
@@ -30,7 +30,8 @@ INSTALLED_APPS = [
     'libsearch',
     'booksearch',
     'recommend',
-    'board'
+    'board',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -109,7 +110,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR/'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
