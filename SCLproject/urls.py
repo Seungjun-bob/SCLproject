90a564d585376bd990c8b5cab7b0ce4b92fb1477
Seungjub-bob <rkdtmdwns0105@gmail.com>
2022-02-28 11:22:57 +0900

    index template

--- SCLproject/urls.py
+++ SCLproject/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +11,4 @@ urlpatterns = [
     path('booksearch/', include('booksearch.urls')),
     path('recommend/', include('recommend.urls')),
     path('board/', include('board.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
