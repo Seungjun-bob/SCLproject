87675661fee7165e924105fad64553cc3c706217
Seungjub-bob <rkdtmdwns0105@gmail.com>
2022-02-28 13:40:44 +0900

    about template

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.register)
+    path('register/', views.register),
+    path('about/', views.about)
 ]
--- account/views.py
+++ account/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 def register(request):
     return render(request, 'register.html')
+
+def about(request):
+    return render(request, 'about.html')
 # Create your views here.
