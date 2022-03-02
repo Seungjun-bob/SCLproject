ef61a6f03aa37186824a3419eb93d86f4e29b511
Jinwoo Kwon <jkwon0122@gmail.com>
2022-03-02 11:45:40 +0900

    board, board/result/

--- board/urls.py
+++ board/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.board, name='board'),
     path('myreview/', views.my_review, name='myreview'),
-    path('submit/', views.submit, name='submit')
+    path('submit/', views.submit, name='submit'),
+    path('result/', views.result, name='result')
 ]
