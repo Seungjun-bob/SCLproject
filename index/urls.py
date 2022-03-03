09d7b70c7ccea1d147136d90541a947661ccf8a1
user <alstjr107@naver.com>
2022-03-03 10:25:15 +0900

    appname수정

--- index/urls.py
+++ index/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
+app_name = 'index'
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('about/', views.about, name='about'),
     path('changepassword/', views.changepassword, name='changepassword'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
     ]
--- libsearch/urls.py
+++ libsearch/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'libsearch'
 urlpatterns = [
     path('', views.libsearch, name='libsearch'),
 ]
