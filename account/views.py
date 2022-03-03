0b7f42692514d34f6a0bbde29df5d14e32a68c3e
user <alstjr107@naver.com>
2022-03-03 09:49:07 +0900

    index -> account하위로 이동

--- account/views.py
+++ account/views.py
@@ -2,13 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 
-def index(request) :
-    context = None
-    print(request.user.is_authenticated)
-    print(request.user)
-    if request.user.is_authenticated:
-        context = {'logineduser': request.user}
-    return render(request, 'index.html', context)
+def index(request):
+    return render(request, 'index.html')
 
 def register(request):
   
