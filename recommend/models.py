68d86fe7c4f3660fb5965d570a3cff6ebe9234f2
Minah <rinette9612@gmail.com>
2022-03-05 16:20:16 +0900

    추천도서 프론트+기본틀

--- recommend/models.py
+++ recommend/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Library(models.Model):
+    lbrry_seq_no = models.CharField(primary_key=True, max_length=45)
+    lbrry_name = models.CharField(max_length=45)
+    gu_code = models.CharField(max_length=10, null=True)
+    code_value = models.CharField(max_length=45)
+    adres = models.CharField(max_length=45)
+    tel_no = models.CharField(max_length=45, null=True)
+    hmpg_url = models.CharField(max_length=200, null=True)
+    op_time = models.CharField(max_length=200, null=True)
+    fdrm_close_date = models.CharField(max_length=200, null=True)
+    lbrry_se_name = models.CharField(max_length=45)
+    xcnts = models.FloatField(null=True)
+    ydnts = models.FloatField(null=True)
--- recommend/views.py
+++ recommend/views.py
@@ -1,7 +1,51 @@
 from django.shortcuts import render
+from libsearch.models import Library
+from django.core.paginator import Paginator
 
 def recommend(request):
-    return render(request, 'recommend.html')
+    search = request.GET.get('search', "")
+    library_div = request.GET.get("lbrry_se_name", "")
+    library_gu = request.GET.get("code_value", "")
+    library_info = Library.objects.all()
+
+    if library_div == "" and library_gu == "" and search == "":
+        total = []
+    elif library_div=='전체' and library_gu=='전체':
+        library_div = ""
+        library_gu = ""
+        total = library_info.filter(lbrry_se_name__icontains=library_div,
+                                    code_value__icontains=library_gu,
+                                    lbrry_name__icontains=search)
+    elif library_div=='전체':
+        library_div = ""
+        total = library_info.filter(lbrry_se_name__icontains=library_div,
+                                    code_value__icontains=library_gu,
+                                    lbrry_name__icontains=search)
+    elif library_gu=='전체':
+        library_gu = ""
+        total = library_info.filter(lbrry_se_name__icontains=library_div,
+                                    code_value__icontains=library_gu,
+                                    lbrry_name__icontains=search)
+    else:
+        total = library_info.filter(lbrry_se_name__icontains=library_div,
+                                    code_value__icontains=library_gu,
+                                    lbrry_name__icontains=search)
+
+    # paging
+    # num = total.count()
+    page = request.GET.get('page', '1')
+
+    paginator = Paginator(total, 5)
+    page_obj = paginator.get_page(page)
+
+    # 결과 출력
+    context = {
+        'total' : page_obj,
+        # 'num' : num, # 도서관 검색 출력 수
+    }
+
+    return render(request, 'recommend.html', context)
+
 
 def detail(request):
     return render(request, 'detail.html')
