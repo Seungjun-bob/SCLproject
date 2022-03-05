91ff555b5a589121e9788b1699dc1fe4813d58b9
Minah <rinette9612@gmail.com>
2022-03-05 17:12:37 +0900

    프론트 분류 수정

--- libsearch/views.py
+++ libsearch/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from libsearch.models import Library
 from django.core.paginator import Paginator
 
-def booksearch(request):
+def libsearch(request):
     search = request.GET.get('search', "")
     library_div = request.GET.get("lbrry_se_name", "")
     library_gu = request.GET.get("code_value", "")
