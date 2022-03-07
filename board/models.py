097c194d7649b01963c247b242b6cfe7eeee813f
Seungjub-bob <rkdtmdwns0105@gmail.com>
2022-03-07 10:40:30 +0900

    board

--- board/models.py
+++ board/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from libsearch.models import Library
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class Review(models.Model):
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    library = models.ForeignKey(Library, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    view_cnt = models.PositiveIntegerField(default=0)
+    date = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    like = models.PositiveIntegerField(default=0)
+    starpoint = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    comments = models.TextField()
--- board/views.py
+++ board/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from .models import Review
+from django.core.paginator import Paginator
 
-
-def board(request):
-    return render(request, 'board.html')
+def board(request) :
+    page = request.GET.get('page', 1)
+    vlist = Review.objects.all()
+    paginator = Paginator(vlist, 3)
+    #print(paginator)
+    vlistpage = paginator.get_page(page)
+    #print(type(vlistpage))
+    #for d in vlistpage :
+    #    print(type(d), d)
+    context = {"vlist": vlistpage}
+    return render(request, 'board.html', context)
 
 
 def my_review(request):
