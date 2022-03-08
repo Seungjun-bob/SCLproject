d2f809364e10151c2a502fe475effa0d87ec40bd
Seungjub-bob <rkdtmdwns0105@gmail.com>
2022-03-08 16:02:46 +0900

    result 수정

--- board/models.py
+++ board/models.py
@@ -1,22 +1,19 @@
 from django.db import models
-from libsearch.models import Library
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-class Review(models.Model):
+class Board(models.Model):
     author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
-    library = models.ForeignKey(Library, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
+    content = models.TextField()
     view_cnt = models.PositiveIntegerField(default=0)
     Cdate = models.DateTimeField(auto_now_add=True)
     Udate = models.DateTimeField(auto_now=True)
-    content = models.TextField()
     like = models.PositiveIntegerField(default=0)
-    starpoint = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     comments = models.TextField()
 
 class Comment(models.Model):
     user = models.ForeignKey("auth.User", on_delete=models.CASCADE)
-    review = models.ForeignKey("Review", on_delete=models.CASCADE)
+    review = models.ForeignKey("Board", on_delete=models.CASCADE)
     comment = models.TextField()
     Cdate = models.DateTimeField(auto_now_add=True)
     Udate = models.DateTimeField(auto_now=True)
